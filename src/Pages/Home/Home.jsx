f0cb4ad46fa2c2013fec61c2356210d7bbdf9a56
Luca Flagiello <trono.9@hotmail.com>
2024-02-24 20:19:58 +0100

    new components

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import Header from '../../Components/Header/Header'
 import Nav from '../../Components/Nav/Nav'
 import Hero from '../../Components/Hero/Hero'
+import FeaturesArea from '../../Components/FeaturesArea/FeaturesAresa'
+import RankingGrid from '../../Components/RankingGrid/RankingGrid'
 
 const Home = () => {
   return (
@@ -8,6 +10,8 @@ const Home = () => {
       <Header />
       <Nav />
       <Hero />
+      <FeaturesArea />
+      <RankingGrid />
     </div>
   )
 }

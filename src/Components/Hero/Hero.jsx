49d8fd45b87bff9b44855c13a9f1c57fc8991b6a
Luca Flagiello <trono.9@hotmail.com>
2024-02-29 18:26:17 +0100

    rebuilt the slider with it's logic to make it more readable and customizable

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,99 +1,77 @@
-import Slider from "react-slick";
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import './hero-slide.css'
 import iphone11ProImg  from '../../Assets/iphone.png'
 import iphoneXrImg  from '../../Assets/iphone-xr.png'
 import iphone12Img  from '../../Assets/iphone-12.png'
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Hero() {
   const theme = useSelector((state) => state.theme.value);
+  const [currSlide, setcurrSlide] = useState(1);
+  const dataSlider = [
+    { 
+      model: 'iPhone 11 Pro Max',
+      newPrice: '450.00',
+      oldPrice: '550.45',
+      img: iphone11ProImg,
+      bgColor: 'bg-hero-bg-pink'
+    },
+    {
+      model: 'iPhone 12 Pro Max',
+      newPrice: '450.00',
+      oldPrice: '550.45',
+      img: iphone12Img,
+      bgColor: 'bg-hero-bg-gray'
+    },
+    { 
+      model: 'Apple iPhone XR',
+      newPrice: '450.00',
+      oldPrice: '550.45',
+      img: iphoneXrImg,
+      bgColor: 'bg-hero-bg-blue'
+    },
+  ]
 
-  console.log(theme)
-
-  let settings = {
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setcurrSlide(prevSlide => (prevSlide === dataSlider.length ? 1 : prevSlide + 1));        
+    }, 5000);
+  
+    return () => clearInterval(interval);
+  }, [currSlide, dataSlider.length]);
+  
   return (
-    <section className={''}>
-      {/*To make the tailwind variables dynamically change works*/}
-      <div className="
-        border-primary-color 
-        hover:text-primary-color 
-        hover:border-primary-color 
-        text-primary-color
-        hidden">
-     </div>
-      <div className="
-        border-primary-color2
-        hover:text-primary-color2
-        text-primary-color2
-        hidden">
-     </div>
-      <div className="
-      border-primary-color3
-      hover:text-primary-color3
-      text-primary-color3  
-        hidden">
-     </div>
-      <div className="
-      border-primary-color4
-      hover:text-primary-color4
-      text-primary-color4
-        hidden">
-     </div>
-      <Slider {...settings}>
-        <div className="w-[600px] h-[407px] bg-hero-bg-pink">
-          <div className="flex justify-center items-center gap-x-[9rem] h-[100%]">  
-            <div>
-              <p className="font-Poppins">Get up to 50&#37; off Today only</p>
-              <h2 className="font-Roboto text-[2.5rem] font-medium">iPhone 11 Pro Max</h2>
-              <div className="flex  items-center  gap-x-3 mt-1 font-Roboto font-medium">
-                <span className={`text-${theme}`}>	&#36;450.00</span>
-                <span className="text-light-gray-price text-[12px] line-through">&#36;550.45</span>
+    <section className={'px-[1rem]'}>
+      <div className="ml-[12.2rem]">
+        <div className="relative flex justify-center items-center max-w-[993px] m-auto h-[407px] overflow-hidden">
+          {dataSlider.map((slide,index) => {
+            const indexSlide = index+1;
+            return (
+              <div key={index} className={currSlide === indexSlide ? `absolute transform translate-x-0 transition duration-[1s] ease w-[993px] h-[100%] ${slide.bgColor} ` : currSlide < indexSlide ? `absolute  transition duration-[1s] ease transform translate-x-[+993px]  h-[100%] w-[100%] ${slide.bgColor}` : `absolute top-0  transition duration-[1s] ease transform translate-x-[-993px] h-[100%] w-[100%] ${slide.bgColor}`}>
+                <div className="flex justify-center items-center gap-x-[9rem] h-[100%]">  
+                  <div>
+                    <p className="font-Poppins">Get up to 50&#37; off Today only</p>
+                    <h2 className="font-Roboto text-[2.5rem] font-medium">{slide.model}</h2>
+                    <div className="flex  items-center  gap-x-3 mt-1 font-Roboto font-medium">
+                      <span className={`text-${theme}`}>	&#36;{slide.newPrice}</span>
+                      <span className="text-light-gray-price text-[12px] line-through">&#36;{slide.oldPrice}</span>
+                    </div>
+                    <button className={`heroBtn font-Poppins border-${theme}  border-[1px] text-[15px] font-medium text-white py-[8px] px-[1.3rem] hover:text-${theme} bg-${theme} rounded-md cursor-pointer hover:bg-transparent transition duration-[.5s] ease mt-[2rem]`}>SHOP NOW</button>
+                  </div>
+                  <img className="h-[280px]" src={slide.img} alt="phone-img" />
+                </div>
               </div>
-              <button className={`heroBtn font-Poppins border-${theme}  border-[1px] text-[15px] font-medium text-white py-[8px] px-[1.3rem] hover:text-${theme} bg-${theme} rounded-md cursor-pointer hover:bg-transparent transition duration-[.5s] ease mt-[2rem]`}>SHOP NOW</button>
-            </div>
-            <img className="h-[280px]" src={iphone11ProImg} alt="" />
-          </div>
+            )
+          })}
+          <ul className="flex gap-x-[.4rem] absolute bottom-[18px]">
+            {dataSlider.map((dot, index) => {
+              let indexDot = index+1;
+              return (
+                <li key={indexDot} onClick={() => setcurrSlide(indexDot)} className={currSlide === indexDot ? `bg-${theme} border-[2px] rounded-[50%] border-${theme} cursor-pointer  h-[12px] w-[12px]` : `border-[2px] rounded-[50%] border-${theme} cursor-pointer  h-[12px] w-[12px]`}></li>    
+              )
+            })}
+          </ul>
         </div>
-        <div className="w-[600px] h-[407px] bg-hero-bg-gray">
-          <div className="flex justify-center items-center gap-x-[9rem] h-[100%]">  
-            <div>
-              <p className="font-Poppins">Get up to 50&#37; off Today only</p>
-              <h2 className="font-Roboto text-[2.5rem] font-medium">iPhone 12 Pro Max</h2>
-              <div className="flex items-center  gap-x-3 mt-1 font-Roboto font-medium">
-                <span className={`text-${theme}`}>&#36;450.00</span>
-                <span className="text-light-gray-price text-[12px] line-through">	&#36;550.45</span>
-              </div>
-              <button className={`font-Poppins bg-${theme} border-${theme} border-[1px] text-[15px] font-medium text-white py-[8px] px-[1.3rem]  rounded-md cursor-pointer hover:text-${theme} hover:bg-transparent transition duration-[.5s] ease mt-[2rem]`}>SHOP NOW</button>
-            </div>
-            <img className="h-[280px]" src={iphoneXrImg} alt="" />
-          </div>
-        </div>
-        <div className="w-[600px] h-[407px] bg-hero-bg-blue">
-          <div className="flex justify-center items-center gap-x-[9rem] h-[100%]">  
-            <div>
-              <p className="font-Poppins">Get up to 50&#37; off Today only</p>
-              <h2 className="font-Roboto text-[2.5rem] font-medium">Apple iPhone XR</h2>
-              <div className="flex  items-center  gap-x-3 mt-1 font-Roboto font-medium">
-                <span className={`text-${theme}`}>	&#36;450.00</span>
-                <span className="text-light-gray-price text-[12px] line-through">	&#36;550.45</span>
-              </div>
-              <button className={`font-Poppins bg-${theme} border-${theme} border-[1px] text-[15px] font-medium text-white py-[8px] px-[1.3rem] rounded-md cursor-pointer  hover:text-${theme} hover:bg-transparent transition duration-[.5s] ease mt-[2rem]`}>SHOP NOW</button>
-            </div>
-            <img className="h-[280px]" src={iphone12Img} alt="" />
-          </div>
-        </div>
-      </Slider>
+      </div>
     </section>
   );
 }

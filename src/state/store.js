c3bf6ce3ade852c894794807905c619b79c27ec5
Luca Flagiello <trono.9@hotmail.com>
2024-03-07 22:11:16 +0100

    added two new states for cart quantity and cart list

--- src/state/store.js
+++ src/state/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import themeSlice from './themeSlice.js'
 import productPreviewSlice from './productPreviewSlice.js'
 import cartQuantitySlice from './cartQuantitySlice.js'
+import cartProductsListSlice from './cartProductsList.js'
 
 export const store = configureStore({
   reducer: {
     theme: themeSlice,
     productPreview: productPreviewSlice,
-    cartQuantity: cartQuantitySlice
+    cartQuantity: cartQuantitySlice,
+    cartProductsList: cartProductsListSlice, 
   }
 })
 

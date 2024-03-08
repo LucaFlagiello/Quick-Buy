da3fad6b966b5e82bcf982942f167d2fad652093
Luca Flagiello <trono.9@hotmail.com>
2024-03-08 10:21:57 +0100

    Added object color variants to change dinamically tailwind classes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import SettingsBtn from "./Components/SettingsBtn/SettingsBtn";
 
 //Tailwind can't process dinamically variables changes, first it needs to see those classes somewhere in source code
 const colorVariants = {
-  primaryColor: 'hover:bg-primary-color bg-primary-color hover:text-primary-color text-primary-color border-primary-color hover:border-primary-color',
-  primaryColor2: 'hover:bg-primary-color2 bg-primary-color2 hover:text-primary-color2 text-primary-color2 border-primary-color2 hover:border-primary-color2',
-  primaryColor3: 'hover:bg-primary-color3 bg-primary-color3 hover:text-primary-color3 text-primary-color3 border-primary-color3 hover:border-primary-color3',
-  primaryColor4: 'hover:bg-primary-color4 bg-primary-color4 hover:text-primary-color4 text-primary-color4 border-primary-color4 hover:border-primary-color4',
+  primaryColor: 'hover:bg-primary-color bg-primary-color hover:text-primary-color text-primary-color border-primary-color hover:border-primary-color group-hover:text-primary-color',
+  primaryColor2: 'hover:bg-primary-color2 bg-primary-color2 hover:text-primary-color2 text-primary-color2 border-primary-color2 hover:border-primary-color2 group-hover:text-primary-color2',
+  primaryColor3: 'hover:bg-primary-color3 bg-primary-color3 hover:text-primary-color3 text-primary-color3 border-primary-color3 hover:border-primary-color3 group-hover:text-primary-colo3',
+  primaryColor4: 'hover:bg-primary-color4 bg-primary-color4 hover:text-primary-color4 text-primary-color4 border-primary-color4 hover:border-primary-color4 group-hover:text-primary-color4',
 }
 
 function App() {

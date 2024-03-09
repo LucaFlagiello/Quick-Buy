a78a2065925bedfbe5c94b05122cd2673d64cd7b
Luca Flagiello <trono.9@hotmail.com>
2024-03-09 16:07:41 +0100

    added two new global states into redux store

--- src/state/store.js
+++ src/state/store.js
@@ -3,13 +3,17 @@ import themeSlice from './themeSlice.js'
 import productPreviewSlice from './productPreviewSlice.js'
 import cartQuantitySlice from './cartQuantitySlice.js'
 import cartProductsListSlice from './cartListSlice.js'
+import wishListSlice from './wishListSlice.js'
+import wishListQuantitySlice from './wishListQuantitySlice.js'
 
 export const store = configureStore({
   reducer: {
     theme: themeSlice,
     productPreview: productPreviewSlice,
-    cartQuantity: cartQuantitySlice,
     cartProductsList: cartProductsListSlice, 
+    cartQuantity: cartQuantitySlice,
+    wishListQuantity: wishListQuantitySlice,
+    wishList: wishListSlice, 
   }
 })
 

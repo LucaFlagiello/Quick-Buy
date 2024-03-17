6d2cd278f615da24baffd17d97c674be16a7dd04
Luca Flagiello <trono.9@hotmail.com>
2024-03-17 20:44:45 +0100

    fixed primary color variant class name

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import Nav from './Components/Nav/Nav';
 import Footer from './Components/Footer/Footer';
 import Copyright from './Components/Copyrigth/Copyright';
 
-//Tailwind can't process dinamically variables changes, first it needs to see those classes somewhere in source code
+/*Tailwind can't process dinamically variables changes, first it needs to see those classes somewhere in source code*/
+
 const colorVariants = {
-  primaryColor: 'hover:bg-primary-color bg-primary-color hover:text-primary-color text-primary-color border-primary-color hover:border-primary-color group-hover:text-primary-color accent-bg-primary-color',
+  primaryColor: 'hover:bg-primary-color bg-primary-color hover:text-primary-color text-primary-color border-primary-color hover:border-primary-color group-hover:text-primary-color accent-primary-color',
   primaryColor2: 'hover:bg-primary-color2 bg-primary-color2 hover:text-primary-color2 text-primary-color2 border-primary-color2 hover:border-primary-color2 group-hover:text-primary-color2 accent-primary-color2',
   primaryColor3: 'hover:bg-primary-color3 bg-primary-color3 hover:text-primary-color3 text-primary-color3 border-primary-color3 hover:border-primary-color3 group-hover:text-primary-colo3 accent-primary-color3',
   primaryColor4: 'hover:bg-primary-color4 bg-primary-color4 hover:text-primary-color4 text-primary-color4 border-primary-color4 hover:border-primary-color4 group-hover:text-primary-color4 accent-primary-color4',
-}
+};
 
 const router = createBrowserRouter([
   {

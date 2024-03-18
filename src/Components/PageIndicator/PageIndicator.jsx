8d9a692b7c401f24f4bee778d602805aa51ffacb
Luca Flagiello <trono.9@hotmail.com>
2024-03-18 21:00:57 +0100

    fixed component width for mobile

--- src/Components/PageIndicator/PageIndicator.jsx
+++ src/Components/PageIndicator/PageIndicator.jsx
@@ -7,7 +7,7 @@ export default function PageIndicator() {
   const page = location.pathname.slice(1);
   
   return (
-    <div className="max-w-[1270px] m-auto px-4 pt-6 md:max-w-[690px] lg:max-w-[1000px]">
+    <div className="max-w-[1265px] m-auto px-4 pt-6 md:max-w-[690px] lg:max-w-[995px]">
       <div className="flex items-center gap-x-2">
         <span className={`icon-[la--home] text-${theme}`}></span>
         <span className="icon-[la--angle-right] text-[14px]"></span>

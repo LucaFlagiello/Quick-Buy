b3c7b667a3005cb156753d3c5048b89a596549bb
Luca Flagiello <trono.9@hotmail.com>
2024-03-19 22:16:45 +0100

    Modified component style

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -19,8 +19,8 @@ export default function NewArrivalsProduct({ img, model, originalPrice, newPrice
 
   return (
     <div className={isProductsList ? 'flex sm:flex-col' : "group"}>
-      <div className={isProductsList ? 'relative w-[300px] bg-[#f3f3f3] rounded-t-md py-6 sm:w-full' :"relative bg-[#f3f3f3] rounded-t-md py-6"}>
-        <img src={img} alt="product-img" className={isProductsList ? "m-auto object-contain h-[220px]" :  "object-contain w-full h-[140px]"}/>
+      <div className={isProductsList ? 'relative w-[250px] bg-[#f3f3f3] rounded-tl-md py-6 sm:w-full' :"relative bg-[#f3f3f3] rounded-t-md py-6"}>
+        <img src={img} alt="product-img" className={isProductsList ? "m-auto object-contain h-[170px]" :  "object-contain w-full h-[140px]"}/>
         <div className={isProductsList ? 'hidden' :'absolute top-0 left-0 flex justify-center items-center w-full group:hover-rounded-t-md h-full group-hover:bg-[#e5e5e58c] group-hover:transition duration-[.3s] ease'}>
           <div className='flex items-center gap-x-4 transition duration-[.3s] ease opacity-0 group-hover:opacity-100'>
             <button onClick={() => productPreviewStatus = true} className={`flex items-center bg-${theme} items-center justify-center rounded-[50%] w-[39px] h-[39px] text-white hover:bg-secondary-color`}><span onClick={() => dispatch(showPreview(true))} className="icon-[material-symbols-light--search] w-[27px] h-[27px]"></span></button>
@@ -36,7 +36,7 @@ export default function NewArrivalsProduct({ img, model, originalPrice, newPrice
            <span className={isProductsList ? `font-medium text-[17px] text-${theme}` : `font-medium text-[15px] text-${theme}`}>&#36;{newPrice}</span>
           <span className={isProductsList ? "line-through text-light-gray-price font-medium text-[15px]" : "line-through text-light-gray-price font-medium text-[13px]"}>&#36;{originalPrice}</span>
         </div>
-        <div className="flex items-center mt-2 gap-x-2">
+        <div className="flex items-center gap-x-2">
           <div>
             <span className={isProductsList ? 'h-[18px] w-[18px] mt-1 icon-[emojione--star]' : "h-[16px] icon-[emojione--star]"}></span>
             <span className={isProductsList ? 'h-[18px] w-[18px] icon-[emojione--star]' : "h-[16px] icon-[emojione--star]"}></span>
@@ -51,16 +51,16 @@ export default function NewArrivalsProduct({ img, model, originalPrice, newPrice
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa enim.
             </p>
             <div className="flex mt-4 gap-x-4 2xs:grid 2xs:gap-y-3 2xs:max-w-[175px]">
-              <button onClick={() => addNewProduct('cart')} className={`flex items-center gap-x-3 bg-${theme} text-white border border-${theme} text-Poppins font-medium py-[10px] px-6 rounded-[4px] text-[15px] hover:bg-transparent hover:text-${theme} transition duration-[.5s] ease group`}>
+              <button onClick={() => addNewProduct('cart')} className={`flex items-center gap-x-3 bg-${theme} text-white border border-${theme} text-Poppins font-medium py-[10px] px-6 rounded-[4px] text-[15px] hover:bg-transparent hover:text-${theme} transition duration-[.5s] ease group sm:px-[1rem]`}>
               <span className={`icon-[cil--cart]  text-white items-center h-[16px] w-[16px] transition duration-[.5s] ease group-hover:text-${theme} transition duration-[.5s] ease`}></span> ADD TO CART
               </button>
               
-              <button onClick={() => addNewProduct('list')} className={`flex items-center gap-x-3 bg-white text-${theme} border border-${theme} text-Poppins font-medium py-[10px] px-8 rounded-[4px] text-[15px] hover:bg-${theme} hover:text-white transition duration-[.5s] ease group`}>
+              <button onClick={() => addNewProduct('list')} className={`flex items-center gap-x-3 bg-white text-${theme} border border-${theme} text-Poppins font-medium py-[10px] px-8 rounded-[4px] text-[15px] hover:bg-${theme} hover:text-white transition duration-[.5s] ease group sm:px-[1rem]`}>
               <span className={`icon-[teenyicons--heart-outline] text-${theme} w-[16px] h-[16px] group-hover:text-white transition duration-[.5s] ease`}></span>WISHLIST
               </button>
             </div>
           </div>
-        <button onClick={() => addNewProduct('cart')} className={ isProductsList ? 'hidden' : `group-hover:translate-y-[-23px] bg-${theme}  group-hover:opacity-100 absolute bottom-0 transform translate-y-0 text-white font-medium font-Poppins text-[14px] border border-${theme} rounded-[4px] py-[7px] px-8 opacity-0 transition-all duration-[.3s] ease hover:text-${theme} hover:bg-white`}>ADD TO CART</button>
+        <button onClick={() => addNewProduct('cart')} className={ isProductsList ? 'hidden' : `group-hover:translate-y-[-23px] bg-${theme}  group-hover:opacity-100 absolute bottom-0 transform translate-y-0 text-white font-medium font-Poppins text-[14px] border border-${theme} rounded-[4px] py-[7px] px-8 opacity-0 transition-all duration-[.3s] ease hover:text-${theme} hover:bg-white sm:px-[12px]`}>ADD TO CART</button>
       </div>
     </div>
   )

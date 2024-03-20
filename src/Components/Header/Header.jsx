4aa34cdb7f8114b880d3814b8a2c2de381a478ea
Luca Flagiello <trono.9@hotmail.com>
2024-03-20 21:16:52 +0100

    added links routes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,22 +2,26 @@ import '../../App.css'
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ component, setIsNavLinkOpen }) => {
   const theme = useSelector((state) => state.theme.value)
 
   return (
-    <header className="flex items-center justify-between py-3 m-auto max-w-[1230px] lg:max-w-[960px] md:hidden sm:hidden">
+    <header className={component === 'mobile-menu' ? '' : "flex items-center justify-between py-4 m-auto max-w-[1230px] lg:max-w-[960px] md:hidden sm:hidden"}>
       <div className='flex items-center gap-x-24 md:gap-x-0'>
-        <span className='text-primary-color cursor-pointer font-Roboto font-bold text-[28px]'>QUICK<span className='text-black'>BUY</span></span>
-
-        <ul className='flex gap-x-8 font-Poppins font-medium text-secondary-color text-[16px]'>
-          <Link to={'/'}><li className={`hover:text-${theme} cursor-pointer transition duration-[.3s] ease`}>Home</li></Link>
-          <Link to={'/Shop/:page'}><li className={`hover:text-${theme} cursor-pointer transition duration-[.3s] ease`}>Shop</li></Link>
-          <li className={`hover:text-${theme} cursor-pointer transition duration-[.3s] ease`}>Contact</li>
+        <Link to={"/"}>
+          <div className={component === 'mobile-menu' ? 'hidden' : ''}>
+            <span className='text-primary-color cursor-pointer font-Roboto font-bold text-  [28px]'>QUICK<span className='text-black'>BUY</span></span>
+          </div>
+        </Link>
+          
+        <ul className={component === 'mobile-menu' ? 'gap-y-3 font-Poppins  text-[1.1rem] w-full' : 'flex gap-x-8 font-Poppins font-medium text-secondary-color text-[16px]'}>
+          <Link to={'/'} onClick={() => setIsNavLinkOpen(false)}><li className={component === 'mobile-menu' ? 'hover:bg-[#f3f3f3] pt-[14px] pb-3 pl-4 border-b' : `hover:text-${theme} cursor-pointer transition duration-[.3s] ease`}>Home</li></Link>
+          <Link to={'/Shop/:page'} onClick={() => setIsNavLinkOpen(false)}><li className={component === 'mobile-menu' ? 'hover:bg-[#f3f3f3] pt-[10px] pb-3 pl-4 border-b' : `hover:text-${theme} cursor-pointer transition duration-[.3s] ease`}>Shop</li></Link>
+          <li className={component === 'mobile-menu' ? 'hover:bg-[#f3f3f3] pt-[10px] pb-3 pl-4 border-b cursor-pointer' : `hover:text-${theme} cursor-pointer transition duration-[.3s] ease`}>Contact</li>
         </ul>
       </div>
 
-      <div className='text-[16px] font-Poppins text-secondary-color font-medium'>
+      <div className={component === 'mobile-menu' ? 'hidden' : 'text-[16px] font-Poppins text-secondary-color font-medium'}>
         <a href='/' className={`hover:text-${theme} cursor-pointer transition duration-[.3s] ease`}>Login</a><span className='hover:text-black'>/</span><a className={`hover:text-${theme} cursor-pointer transition duration-[.3s] ease`} href='/'>Register</a>
       </div>
     </header>
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,46 +1,21 @@
-import brand1Img from '../../Assets/brand-1.png'
-import brand2Img from '../../Assets/brand-2.png'
-import brand3Img from '../../Assets/brand-3.png'
-import brand4Img from '../../Assets/brand-4.png'
-import brand5Img from '../../Assets/brand-5.png'
-import brand6Img from '../../Assets/brand-6.png'
-import brand7Img from '../../Assets/brand-7.png'
 import { nanoid } from '@reduxjs/toolkit';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from "react-redux";
 import { decrement } from '../../state/cartQuantitySlice';
 import { removeCartProduct } from '../../state/cartListSlice';
-import { useLocation } from 'react-router-dom';
 import Categories from '../../Data/navSideCategories';
-import NavSubCategories from '../NavSubCategories/NavSubCategories';
-import SideCategoriesBrand from '../SideCategoriesBrand/SideCategoriesBrand';
-import SearchCategory from '../SearchCategory/SearchCategory'
+import SearchCategory from '../SearchCategory/SearchCategory';
+import NavAsideCategories from '../NavAsideCategories/NavAsideCategories';
 
 const Nav = () => {
-  const [navSidecategories, setNavSideCategories] = useState(Categories);
   const [isMouseOnAccount, setIsMouseOnAccount] = useState(false);
-  const [sideCategorySelected, setSideCategorySelected] = useState('');
-  const location = useLocation();
-  const isSideCategoriesOpen = location.pathname === '/' ? true : false;
   const theme = useSelector((state) => state.theme.value);
   const cartQuantity = useSelector((state) => state.cartQuantity);
   const cartProductsList = useSelector((state) => state.cartProductsList);
   const wishListQuantity = useSelector((state) => state.wishListQuantity);
   const dispatch = useDispatch();
   let carSubTotal = 0; 
-
-  const sideCategoriesBrands = [{img: brand1Img}, {img: brand2Img}, {img: brand3Img}, {img: brand4Img}, {img: brand5Img}, {img: brand6Img}, {img: brand7Img}, {img: brand1Img}, {img: brand2Img}, {img: brand3Img}, {img: brand4Img}, {img: brand5Img}, {img: brand6Img}, {img: brand7Img}];
-
-
-  const rightArrowCategoriesAnimation = (value) => {
-    setSideCategorySelected(value);
-    let updateCategories = navSidecategories.map(item => {
-      if(item.category === value) {
-        return { ...item, isOnCategory: true , showSideContainer: true};
-      } return { ...item, isOnCategory: false, showSideContainer: false };
-    })
-    setNavSideCategories(updateCategories);
-  };
 
   const removeCartListProduct = (product) => {
     dispatch(decrement(product));
@@ -50,43 +25,15 @@ const Nav = () => {
   return (
     <nav className={`bg-${theme} box-border h-[65px] flex justify-center w-full`}>
       <div className='box-border h-[65px] flex justify-center items-center sm:justify-between sm:gap-x-0 sm:px-2 w-full md:justify-between md:px-7 gap-x-[4.7rem] lg:gap-x-[2rem]'>
-        <span className='text-white cursor-pointer font-Roboto font-bold text-[28px] xl:hidden lg:hidden'>QUICK<span className='text-black'>BUY</span></span>
+        <Link to={"/"}>
+          <span className='text-white cursor-pointer font-Roboto font-bold text-[28px] xl:hidden lg:hidden'>QUICK<span className='text-black'>BUY</span></span>
+        </Link>
         <div className="bg-secondary-color relative flex items-center gap-x-2 h-10 px-10 py-6 rounded-[5px] group sm:hidden md:hidden">
           <span className="icon-[la--bars] text-white text-[22px]"></span>
           <h3 className='font-medium text-white font-Poppins text-md'>All categories</h3>
           <div className='w-[220px] absolute h-[12px] left-0  bottom-[-12px]'>  
           </div>
-          <ul className={isSideCategoriesOpen ? 'visible transition ease duration-[.2s] absolute w-full text-gray-800 text-[15px] font-Poppins bottom-0 left-0 top-14 shadow-light-shadow h-fit pt-[14px] pb-[13px] z-10 sm:hidden' : 'invisible bg-white opacity-0 transition ease duration-[.3s] absolute w-full text-gray-800 text-[15px] font-Poppins bottom-[0px] left-0 top-[0px] transform translate-y-[100px] group-hover:translate-y-[58px] group-hover:opacity-100 group-hover:visible shadow-light-shadow h-fit pt-[14px] pb-[13px] z-10 sm:hidden'}>
-            {navSidecategories.map((item, i) => 
-              <li value={item.category} onMouseEnter={() => rightArrowCategoriesAnimation(item.category)} onMouseLeave={() => rightArrowCategoriesAnimation('')} key={item.category} className='flex items-center border-b-[1px] border-dotted gap-x-3 py-3 pl-8 hover:bg-gray-100 last:border-b-0 transition duration-[.2s] cursor-pointer'>
-                <span className={`${item.icon} text-tertiary-color text-[1.5em]`}></span>
-                <span>{item.category}</span>
-                <span className={item.isOnCategory ? 'absolute right-[20px] icon-[la--angle-right] transform translate-x-[3px] transition-all duration-[.4s] ease' : 'absolute right-[20px] transform transalte-x-0 icon-[la--angle-right] transition-all duration-[.4s] ease'}></span>
-                <div className={item.showSideContainer ? 'absolute top-[1px] bottom-0 right-[-1023px] h-[407px] w-[1022px] border-[2px] border-[#e9e4e4] rounded-[6px] bg-white translate-x-0 transition duration-[.4s] ease': 'absolute top-[1px] bottom-0 invisible translate-x-[20px] opacity-0'}>
-                <div className='absolute grid grid-cols-3 gap-y-[1rem] gap-x-[4rem] p-[1rem] px-[2.5rem]'>
-                  {navSidecategories.map(item => {
-                    if(item.category === sideCategorySelected) {
-                      return item.sideCategories.map(list => {
-                        return (
-                          <ul key={nanoid()} className= 'leading-[28px]'>
-                            <NavSubCategories theme={theme} list={list}/>
-                          </ul>
-                        )
-                      })
-                    } return null
-                  })} 
-                  <div className={sideCategorySelected ? 'absolute top-[20px] right-[-250px] grid grid-cols-2 gap-2' : 'hidden'}>
-                    {sideCategoriesBrands.map(brand => {
-                      return (
-                        <SideCategoriesBrand key={nanoid()} brandImg={brand.img} />  
-                      )
-                    })}
-                  </div>
-                </div>
-              </div>
-            </li>
-            )}
-          </ul>
+          <NavAsideCategories theme={theme} />
         </div>
         <div className='flex h-[44px] font-Poppins md:hidden sm:hidden'>
           <SearchCategory categories={Categories} />

1d5f2cf1b3a2ebbd9e418cd57e0d74cae29c52b9
Luca Flagiello <trono.9@hotmail.com>
2024-03-23 20:28:44 +0100

    Fixed a bug where react component was receiving an object as a child

--- src/Components/NavSubCategories/NavSubCategories.jsx
+++ src/Components/NavSubCategories/NavSubCategories.jsx
@@ -1,9 +1,7 @@
-
-
 export default function NavSideCategories({ theme, list }) {
   return (
     <>
-      <li className='text-[18px] font-Roboto font-medium mb-1'>{list.headerCategory}</li>
+      <li className='text-[18px] font-Roboto font-medium mb-1'>{list.headerCategory.name}</li>
       <li className={`font-Poppins hover:text-${theme} cursor-pointer transition duration-[.2s] ease`}>{list.subCategory1}</li>
       <li className={`font-Poppins hover:text-${theme} cursor-pointer transition duration-[.2s] ease`}>{list.subCategory2}</li>
       <li className={`font-Poppins hover:text-${theme} cursor-pointer transition duration-[.2s] ease`}>{list.subCategory3}</li>

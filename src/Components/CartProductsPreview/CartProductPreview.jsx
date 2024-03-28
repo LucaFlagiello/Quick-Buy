316a2c3294b58726f004e9155ee7193a1bf0e6f4
Luca Flagiello <trono.9@hotmail.com>
2024-03-28 21:39:43 +0100

    Built component to make code more readable

--- src/Components/CartProductsPreview/CartProductPreview.jsx
+++ src/Components/CartProductsPreview/CartProductPreview.jsx
@@ -3,32 +3,34 @@ import { decrement } from '../../state/cartQuantitySlice';
 import { removeCartProduct } from '../../state/cartListSlice';
 import { useSelector, useDispatch } from "react-redux";
 
-export default function CartProductPreview({ theme, carSubTotal, mobileStyle, closeLinks, isCartOpen }) {
+export default function CartProductPreview({ theme, mobileStyle, isCartOpen }) {
   const dispatch = useDispatch();
   const cartQuantity = useSelector((state) => state.cartQuantity);
   const cartProductsList = useSelector((state) => state.cartProductsList);
+  let carSubTotal = 0;
   
   const removeCartListProduct = (product) => {
     dispatch(decrement(product));
     dispatch(removeCartProduct(product));
   };
-  
+
   return (
-    <div className={mobileStyle ? 'relative grid gap-y-[2px] text-center cursor-pointer  mt-1' : 'relative grid gap-y-[2px] text-center cursor-pointer group sm:hidden md:hidden'}>
+    <div className={mobileStyle ? 'relative grid gap-y-[2px] text-center cursor-pointer mt-1' : 'relative grid gap-y-[2px] text-center cursor-pointer group sm:hidden md:hidden'}>
       <div className='relative grid'>
         <span className={mobileStyle ? "icon-[cil--cart] items-center h-[22px] w-[22px]" : "icon-[cil--cart] text-white items-center h-[28px] w-[28px]"}></span>
-        <span className='absolute cursor-pointer top-[-5px] right-[-8px] bg-black text-white text-[9px] font-medium rounded-full flex justify-center items-center w-[15px] h-[15px]'>{cartQuantity}</span>
+        <span className={mobileStyle ? `absolute cursor-pointer top-[-5px] right-[-8px] bg-${theme} text-white text-[9px] font-medium rounded-full flex justify-center items-center w-[15px] h-[15px]` : `absolute cursor-pointer top-[-5px] right-[-8px] bg-black text-white text-[9px] font-medium rounded-full flex justify-center items-center w-[15px] h-[15px]`}>{cartQuantity}</span>
       </div>
-    <div className={mobileStyle ? isCartOpen ? 'fixed top-0 bottom-0 left-0 w-[310px] bg-white z-10 pb-4 duration-600 ease transform duration-500 ease translate-x-[300px]' : 'fixed top-0 bottom-0 left-0 w-[310px] bg-white z-10 pb-4 duration-600 ease transform duration-500 ease translate-x-0' : 'absolute top-[53px] right-[-5px] w-[310px] bg-white z-10 pb-4 duration-600 ease opacity-0 transform duration-500 ease translate-y-4 shadow-default invisible group-hover:translate-y-0 group-hover:visible '}>
+    <div className={mobileStyle ? isCartOpen ? 'fixed top-0 bottom-0 left-0 w-[310px] bg-white z-10 pb-4 duration-600 ease transform duration-500 ease translate-x-0' : 'fixed top-0 bottom-0 left-0 w-[310px] bg-white z-10 pb-4 duration-600 ease transform duration-500 ease translate-x-[-400px]' : 'absolute top-[53px] right-[-5px] w-[310px] bg-white z-10 pb-4 duration-600 ease opacity-0 transform duration-500 ease translate-y-4 shadow-default invisible group-hover:translate-y-0 group-hover:visible group-hover:opacity-100'}>
+      <div className={mobileStyle ? `flex items-center justify-center text-white font-Roboto font-medium h-[50px] text-[1.3rem] bg-${theme}` : `hidden`}>
+        <h2>Cart</h2>
+      </div>
       <div className='mt-6 ml-6 font-medium text-start font-Roboto'>{cartQuantity} Items</div>
-      
       {cartProductsList.length > 0 ? 
-        <div className='flex flex-col py-3 mx-6 mt-1 border-y gap-y-6 border-[#d8d8d8]'>
+        <div className={mobileStyle ? 'flex flex-col py-3 mx-6 mt-1 border-y gap-y-6 border-[#d8d8d8] h-[720px] overflow-auto' : 'flex flex-col py-3 mx-6 mt-1 border-y gap-y-6 border-[#d8d8d8]'}>
           {cartProductsList.map((product) => {
             carSubTotal += parseFloat(product.newPrice * product.quantity);
             carSubTotal= parseFloat(carSubTotal.toFixed(2));
             const shortModelName = product.model.length > 15 ? product.model.slice(0, 15) + '...' : product.model;
-            
             return (
               <div key={nanoid()} className='relative flex gap-x-4'>
                 <img className='w-[60px] object-fit' src={product.img} alt="product" />
@@ -50,10 +52,10 @@ export default function CartProductPreview({ theme, carSubTotal, mobileStyle, cl
         </div>
         <div className='flex justify-center mt-4 gap-x-4'>
           <button className={`bg-${theme} text-white font-Poppins py-[9px] px-[22px] border border-${theme} rounded-[4px] font-medium text-[14px] transition duration-500 ease hover:bg-transparent hover:text-${theme}`}>VIEW CART</button>
-          <button className={`text-${theme} font-Poppins py-[9px] px-[22px] border border-${theme} rounded-[4px] font-medium text-[14px] transition duration-500 ease hover:bg-${theme}  hover:text-white`}>CHECKOUT</button>
+          <button className={`text-${theme} font-Poppins py-[9px] px-[22px] border border-${theme} rounded-[4px] font-medium text-[14px] transition duration-500 ease hover:bg-${theme} hover:text-white`}>CHECKOUT</button>
         </div>
     </div>
     <span className={mobileStyle ? 'text-[11px] font-Poppins' : 'text-white text-[11px] font-Poppins'}>cart</span>
   </div>
   )
-}
+};

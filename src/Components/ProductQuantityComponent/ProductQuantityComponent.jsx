39e6b521c0f93358461fa72b6188171cef6b2eed
Luca Flagiello <trono.9@hotmail.com>
2024-05-10 20:21:01 +0200

    upgraded logic price for adding and removing products

--- src/Components/ProductQuantityComponent/ProductQuantityComponent.jsx
+++ src/Components/ProductQuantityComponent/ProductQuantityComponent.jsx
@@ -1,12 +1,32 @@
-export default function ProductQuantityComponent({ productQuantity, setProductQuantity }) {
+import { useDispatch } from "react-redux"
+import { addCartProduct, decrementCartProduct } from "../../state/cartListSlice";
+
+export default function ProductQuantityComponent({ page, product, productQuantity, calcolateTotalPrice}) {
+  
+  const dispatch = useDispatch();
+
+  const handleProductIncrement = () => {
+    dispatch(addCartProduct(product));
+    calcolateTotalPrice(product);
+  };  
+  
+  const handleProductDecrement = () => {
+    dispatch(decrementCartProduct(product));
+    calcolateTotalPrice();
+  };
+
   return (
     <div className='my-5'>
-      <h5 className='font-Roboto'>Quantity</h5>
+      {
+        page === 'shopping-cart' 
+        ? null 
+        : <h5 className='font-Roboto'>Quantity</h5>
+      }
       <div className='flex items-center mt-1'>
-        <button onClick={() => productQuantity > 0 ? setProductQuantity(prevQuantity => prevQuantity - 1) : null} className='flex items-center justify-center border border-[1px] w-[32px] h-[32px]'><span className="icon-[la--minus] h-[17px] w-[17px]"></span></button>
-        <div className='flex items-center justify-center border border-[1px] w-[32px] h-[32px]'>{productQuantity}</div>
-        <button onClick={() => setProductQuantity(prevQuantity => prevQuantity + 1)} className='flex items-center justify-center border border-[1px]  w-[32px] h-[32px]'><span className="icon-[la--plus] h-[17px] w-[17px]"></span></button>
+        <button onClick={handleProductDecrement} className='flex items-center justify-center border-[1px] w-[32px] h-[32px]'><span className="icon-[la--minus] h-[17px] w-[17px]"></span></button>
+         <div className='flex items-center justify-center border-[1px] w-[32px] h-[32px]'>{productQuantity}</div> 
+        <button onClick={handleProductIncrement} className='flex items-center justify-center border-[1px]  w-[32px] h-[32px]'><span className="icon-[la--plus] h-[17px] w-[17px]"></span></button>
       </div>
     </div>
   )
-}
+}; 

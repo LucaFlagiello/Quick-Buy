24f5cf9134eecc08776589d541856e8471c23ed1
Luca Flagiello <trono.9@hotmail.com>
2024-05-14 20:08:21 +0200

    upgrade product quantity component logic for different pages

--- src/Components/ProductQuantityComponent/ProductQuantityComponent.jsx
+++ src/Components/ProductQuantityComponent/ProductQuantityComponent.jsx
@@ -1,18 +1,30 @@
 import { useDispatch } from "react-redux"
 import { addCartProduct, decrementCartProduct } from "../../state/cartListSlice";
+import { useState } from "react";
 
 export default function ProductQuantityComponent({ page, product, productQuantity, calcolateTotalPrice}) {
   
+  const [productPageQuantity, setProductPageQuantity ] = useState(0)
+  
   const dispatch = useDispatch();
+  const quantity = page === 'shopping-cart' ? productQuantity : productPageQuantity;
 
   const handleProductIncrement = () => {
-    dispatch(addCartProduct(product));
-    calcolateTotalPrice(product);
+    if(page === 'shopping-cart') { 
+      dispatch(addCartProduct(product));
+      calcolateTotalPrice(product);
+    } else {
+      setProductPageQuantity(prev => prev + 1);
+    };
   };  
   
   const handleProductDecrement = () => {
-    dispatch(decrementCartProduct(product));
-    calcolateTotalPrice();
+    if(page === 'shopping-cart') { 
+      dispatch(decrementCartProduct(product));
+      calcolateTotalPrice();
+    } else if(productPageQuantity !== 0) {
+        setProductPageQuantity(prev => prev - 1); 
+      };
   };
 
   return (
@@ -24,7 +36,7 @@ export default function ProductQuantityComponent({ page, product, productQuantit
       }
       <div className='flex items-center mt-1'>
         <button onClick={handleProductDecrement} className='flex items-center justify-center border-[1px] w-[32px] h-[32px]'><span className="icon-[la--minus] h-[17px] w-[17px]"></span></button>
-         <div className='flex items-center justify-center border-[1px] w-[32px] h-[32px]'>{productQuantity}</div> 
+         <div className='flex items-center justify-center border-[1px] w-[32px] h-[32px]'>{quantity}</div> 
         <button onClick={handleProductIncrement} className='flex items-center justify-center border-[1px]  w-[32px] h-[32px]'><span className="icon-[la--plus] h-[17px] w-[17px]"></span></button>
       </div>
     </div>

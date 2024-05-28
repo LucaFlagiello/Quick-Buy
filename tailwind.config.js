1a4b3ead25779fcd60753388fbf7caba8c801dd7
Luca Flagiello <trono.9@hotmail.com>
2024-05-28 19:35:01 +0200

    Added custom style

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,10 @@ module.exports = {
       screens: {
         '2xs': {'max':'368px'},
         'xs': {'min':'369px', 'max':'598px'},
-        'sm': {'max':'743px'},
+        'sm': {'max':'742px'},
         'md': {'min':'743px', 'max': '1012px'},
         'lg': {'min':'1013px', 'max':'1284px'},
-        'xl': {'min':'1284px'}
+        'xl': {'min':'1285px'}
       },
       colors: {
         'primary-color': '#FD3D57',
@@ -38,6 +38,7 @@ module.exports = {
         "shop-shadow": '0 0 5px rgba(0 0 0 / 0.1);',
         "light-shadow": '0 0 5px rgba(0 0 0 / 0.3);',
         "default": '0px 5px 11px rgba(0 0 0 / .32)',
+        "contact": '0px 0px 16px rgba(0, 0, 0, 0.06)',
       },
       animation: {
         'spin-slow': 'spin 3.5s linear infinite',

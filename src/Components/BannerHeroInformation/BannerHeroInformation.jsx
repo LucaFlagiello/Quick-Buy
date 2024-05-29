cb24bb4786a3c94b0f9acbaef11a509e300523cc
Luca Flagiello <trono.9@hotmail.com>
2024-05-29 17:49:01 +0200

    rechanged img path due to vercel deployment error

--- src/Components/BannerHeroInformation/BannerHeroInformation.jsx
+++ src/Components/BannerHeroInformation/BannerHeroInformation.jsx
@@ -1,8 +1,9 @@
 import PageIndicator from "../PageIndicator/PageIndicator"
+import bgiMG from '/Users/trono/quick-buy/src/Assets/coffee-work-productivity.jpg'
 
 export default function BannerHeroInformation({ pageComponent }) {
   return (
-    <div className="relative h-[350px] bg-center bg-cover bg-[url('/Users/trono/quick-buy/public/coffee-work-productivity.jpg')] sm:h-[280px]">
+    <div className="relative h-[350px] bg-center bg-cover bg-[url('/Users/trono/quick-buy/src/Assets/coffee-work-productivity.jpg')] sm:h-[280px]">
       <div className="absolute inset-0 grid items-center justify-center h-10 grid-cols-1 m-auto text-[white]">
         <h2 className=' text-[1.8rem] uppercase text-center font-Roboto font-medium 2xs:text-[1.2rem] xs:text-[1.2rem] md:pl-4'>{pageComponent}</h2>
         <PageIndicator page={pageComponent}/>

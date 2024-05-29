7a05ebb2a0844df7b23a15c2bb780ad55a3024d7
Luca Flagiello <trono.9@hotmail.com>
2024-05-29 19:01:25 +0200

    changed path

--- src/Components/BannerHeroInformation/BannerHeroInformation.jsx
+++ src/Components/BannerHeroInformation/BannerHeroInformation.jsx
@@ -1,12 +1,19 @@
-import PageIndicator from "../PageIndicator/PageIndicator"
+import React from 'react';
+import PageIndicator from "../PageIndicator/PageIndicator";
+import coffeeWorkProductivity from '../../Assets/coffee-work-productivity.jpg';
 
 export default function BannerHeroInformation({ pageComponent }) {
   return (
-    <div className="relative h-[350px] bg-center bg-cover bg-[url('/Users/trono/quick-buy/src/Assets/coffee-work-productivity.jpg')] sm:h-[280px]">
-      <div className="absolute inset-0 grid items-center justify-center h-10 grid-cols-1 m-auto text-[white]">
-        <h2 className=' text-[1.8rem] uppercase text-center font-Roboto font-medium 2xs:text-[1.2rem] xs:text-[1.2rem] md:pl-4'>{pageComponent}</h2>
-        <PageIndicator page={pageComponent}/>
+    <div
+      className="relative h-[350px] bg-center bg-cover sm:h-[280px]"
+      style={{ backgroundImage: `url(${coffeeWorkProductivity})` }}
+    >
+      <div className="absolute inset-0 grid items-center justify-center h-10 grid-cols-1 m-auto text-white">
+        <h2 className="text-[1.8rem] uppercase text-center font-Roboto font-medium 2xs:text-[1.2rem] xs:text-[1.2rem] md:pl-4">
+          {pageComponent}
+        </h2>
+        <PageIndicator page={pageComponent} />
       </div>
     </div>
-  )
-};
+  );
+}
